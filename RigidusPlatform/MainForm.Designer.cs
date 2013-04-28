071cd2cbbe22bb6316248d34bec45ad568f9124c
Natzn <jannikroesch@gmail.com>
2013-04-28 13:41:18 +0200

    Initial Code commit

---
+++ RigidusPlatform/MainForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Rigidus.Platform
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rigidusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hacksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hacksToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.friendsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.friendlistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.puplicChatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coDBOIIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coDMW3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crossfireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crossfireEUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crossfireNAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crossfireUKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rigidusToolStripMenuItem,
+            this.hacksToolStripMenuItem,
+            this.friendsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(641, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rigidusToolStripMenuItem
+            // 
+            this.rigidusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.rigidusToolStripMenuItem.Name = "rigidusToolStripMenuItem";
+            this.rigidusToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.rigidusToolStripMenuItem.Text = "Rigidus";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // hacksToolStripMenuItem
+            // 
+            this.hacksToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newsToolStripMenuItem,
+            this.hacksToolStripMenuItem1});
+            this.hacksToolStripMenuItem.Name = "hacksToolStripMenuItem";
+            this.hacksToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.hacksToolStripMenuItem.Text = "Hacks";
+            // 
+            // newsToolStripMenuItem
+            // 
+            this.newsToolStripMenuItem.Name = "newsToolStripMenuItem";
+            this.newsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newsToolStripMenuItem.Text = "News";
+            // 
+            // hacksToolStripMenuItem1
+            // 
+            this.hacksToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.coDBOIIToolStripMenuItem,
+            this.coDMW3ToolStripMenuItem,
+            this.crossfireToolStripMenuItem});
+            this.hacksToolStripMenuItem1.Name = "hacksToolStripMenuItem1";
+            this.hacksToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.hacksToolStripMenuItem1.Text = "Hacks";
+            // 
+            // friendsToolStripMenuItem
+            // 
+            this.friendsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.friendlistToolStripMenuItem,
+            this.searchToolStripMenuItem,
+            this.puplicChatToolStripMenuItem});
+            this.friendsToolStripMenuItem.Name = "friendsToolStripMenuItem";
+            this.friendsToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.friendsToolStripMenuItem.Text = "Friends";
+            // 
+            // friendlistToolStripMenuItem
+            // 
+            this.friendlistToolStripMenuItem.Name = "friendlistToolStripMenuItem";
+            this.friendlistToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.friendlistToolStripMenuItem.Text = "Friendlist";
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.searchToolStripMenuItem.Text = "Search Friend";
+            // 
+            // puplicChatToolStripMenuItem
+            // 
+            this.puplicChatToolStripMenuItem.Name = "puplicChatToolStripMenuItem";
+            this.puplicChatToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.puplicChatToolStripMenuItem.Text = "Puplic Chat";
+            // 
+            // coDBOIIToolStripMenuItem
+            // 
+            this.coDBOIIToolStripMenuItem.Name = "coDBOIIToolStripMenuItem";
+            this.coDBOIIToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.coDBOIIToolStripMenuItem.Text = "CoD BO II";
+            // 
+            // coDMW3ToolStripMenuItem
+            // 
+            this.coDMW3ToolStripMenuItem.Name = "coDMW3ToolStripMenuItem";
+            this.coDMW3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.coDMW3ToolStripMenuItem.Text = "CoD MW 3";
+            // 
+            // crossfireToolStripMenuItem
+            // 
+            this.crossfireToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crossfireEUToolStripMenuItem,
+            this.crossfireNAToolStripMenuItem,
+            this.crossfireUKToolStripMenuItem});
+            this.crossfireToolStripMenuItem.Name = "crossfireToolStripMenuItem";
+            this.crossfireToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.crossfireToolStripMenuItem.Text = "Crossfire";
+            // 
+            // crossfireEUToolStripMenuItem
+            // 
+            this.crossfireEUToolStripMenuItem.Name = "crossfireEUToolStripMenuItem";
+            this.crossfireEUToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.crossfireEUToolStripMenuItem.Text = "Crossfire EU";
+            // 
+            // crossfireNAToolStripMenuItem
+            // 
+            this.crossfireNAToolStripMenuItem.Name = "crossfireNAToolStripMenuItem";
+            this.crossfireNAToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.crossfireNAToolStripMenuItem.Text = "Crossfire NA";
+            // 
+            // crossfireUKToolStripMenuItem
+            // 
+            this.crossfireUKToolStripMenuItem.Name = "crossfireUKToolStripMenuItem";
+            this.crossfireUKToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.crossfireUKToolStripMenuItem.Text = "Crossfire UK";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 348);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.Text = "Rigidus";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rigidusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hacksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hacksToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem friendsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem friendlistToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem puplicChatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coDBOIIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coDMW3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crossfireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crossfireEUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crossfireNAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crossfireUKToolStripMenuItem;
+    }
+}
